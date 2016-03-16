0ba74ccf91bdb5b5ea61ecffd7140ecef1d82278
Giuseppe Crinò <giuscri@gmail.com>
2016-03-16 09:23:58 +0100

    Remove db.serialize() wrapper
    Queries don't need to be serialized

--- src/main.js
+++ src/main.js
@@ -4,36 +4,34 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./etc/main.db');
 
 var server = http.createServer(function(req, res) {
-    db.serialize(function() {
-        if (req.url.match(/\/api\/episode\/?$/)) {
-            db.get('select count(*) as n_episodes from main', function(err, row) {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                return res.end(JSON.stringify({n_episodes: row['n_episodes']}));
-            });
-        }
+    if (req.url.match(/\/api\/episode\/?$/)) {
+        db.get('select count(*) as n_episodes from main', function(err, row) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify({n_episodes: row['n_episodes']}));
+        });
+    }
 
-        else if (req.url.match(/\/api\/episode\/\d{1,2}\/?$/)) {
-            var episode_id = req.url.match(/\/api\/episode\/(\d{1,2})\/?$/)[1];
+    else if (req.url.match(/\/api\/episode\/\d{1,2}\/?$/)) {
+        var episode_id = req.url.match(/\/api\/episode\/(\d{1,2})\/?$/)[1];
 
-            db.get(`select url from main where id=${episode_id}`, function(err, row) {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                if (row) {
-                    return res.end(JSON.stringify({
-                        'valid': true,
-                        'video_url': row['url'],
-                        'authorization_token': '...',
-                    }));
-                }
+        db.get(`select url from main where id=${episode_id}`, function(err, row) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            if (row) {
+                return res.end(JSON.stringify({
+                    'valid': true,
+                    'video_url': row['url'],
+                    'authorization_token': '...',
+                }));
+            }
 
-                return res.end(JSON.stringify({'valid': false}));
-            });
-        }
-
-        else {
-            res.writeHead(404, {'Content-Type': 'application/json'});
             return res.end(JSON.stringify({'valid': false}));
-        }
-    });
+        });
+    }
+
+    else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify({'valid': false}));
+    }
 });
 
 module.exports = server;
