820c2c9a9d56ff2dd88eb91ea40954b6b469e7f7
Giuseppe Crinò <giuscri@gmail.com>
2016-03-16 09:26:20 +0100

    Handle only URL that start with /api.*
    Previously it handled /any/possible/url/api.* too

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./etc/main.db');
 
 var server = http.createServer(function(req, res) {
-    if (req.url.match(/\/api\/episode\/?$/)) {
+    if (req.url.match(/^\/api\/episode\/?$/)) {
         db.get('select count(*) as n_episodes from main', function(err, row) {
             res.writeHead(200, {'Content-Type': 'application/json'});
             return res.end(JSON.stringify({n_episodes: row['n_episodes']}));
         });
     }
 
-    else if (req.url.match(/\/api\/episode\/\d{1,2}\/?$/)) {
+    else if (req.url.match(/^\/api\/episode\/\d{1,2}\/?$/)) {
         var episode_id = req.url.match(/\/api\/episode\/(\d{1,2})\/?$/)[1];
 
         db.get(`select url from main where id=${episode_id}`, function(err, row) {
